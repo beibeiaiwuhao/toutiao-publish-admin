0b88c34c44ed39f2b14cded44c09806c65e6dfb0
吴昊 <724803869@qq.com>
2021-09-09 19:11:12 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 创建router 根实例
+// 把 router 配置到实例中
+// 通过render 方法把App 根组件渲染到#app 入口节点
 new Vue({
   router,
   render: h => h(App)
